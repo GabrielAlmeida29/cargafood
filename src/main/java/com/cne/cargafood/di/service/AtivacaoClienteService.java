09e2a95cae31d0d5d1f23cb26560470b95e93c76
Gabriel Santos de Almeida <1gabrielsantosdealmeida@gmail.com>
2023-09-15 02:33:24 -0300

    Metodos callback e ciclo de vida de beans

--- src/main/java/com/cne/cargafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/cne/cargafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,8 @@
 package com.cne.cargafood.di.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,12 +11,22 @@ import com.cne.cargafood.di.notificacao.NivelUrgencia;
 import com.cne.cargafood.di.notificacao.Notificador;
 import com.cne.cargafood.di.notificacao.TipoDoNotificador;
 
-@Component
+//@Component
 public class AtivacaoClienteService {
 	
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
+	
+	//@PostConstruct
+	public void init() {
+		System.out.println("INIT" + notificador);
+	}
+	
+	//@PreDestroy
+	public void destroy() {
+		System.out.println("DESTROY");
+	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
